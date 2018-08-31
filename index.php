08987757fb5d6e4f2ddbf2c63fe3f028e948ef21
Saiful Islam <codersaiful@gmail.com>
2018-09-01 00:21:54 +0600

    Abstract Class complete Here. We will start next

--- index.php
+++ index.php
@@ -1,38 +1,23 @@
 <?php
 
-interface School {
-    public function mySchool();
-    public function mySchoolClass($class_name);
-}
-interface College{
-    public function myCollege();
-    public function myCollegeClass($class_name);
-}
-class Teacher implements School, College{
-    public $name;
-    public function __construct($name) {
-        $this->name = $name;
+abstract class School{
+    public function details(){
+        return "It is inside Abstact Class<br>";
     }
-    public function mySchool() {
-        echo 'Welcome To School. Mr. ' . $this->name . '<br>';
-    }
-    public function myCollege() {
-        echo 'Welcome To my College. Mr. ' . $this->name . '<br>';
-    }
-    public function mySchoolClass($class_name) {
-        echo 'I read in School Class ' . $class_name . '<br>';
-    }
-    
-    public function myCollegeClass($class_name) {
-        echo 'I read in College Class ' . $class_name . '<br>';
-    }
-    
-    
-    
+    public abstract function other();
 }
 
-$tchr = new Teacher("Saiful Islam");
-$tchr->mySchool();
-$tchr->myCollege();
-$tchr->mySchoolClass("Ten");
-$tchr->myCollegeClass("Intermidiate");
+class Student extends School{
+    public $name = "Saiful islam";
+    public $age = "33";
+    public function details() {
+        echo "Name: " . $this->name . " " . parent::details();
+    }
+    public function other(){
+        echo "This is Other Name. Age is: {$this->age}<br>";
+    }
+}
+
+$st = new Student();
+$st->details();
+$st->other();
